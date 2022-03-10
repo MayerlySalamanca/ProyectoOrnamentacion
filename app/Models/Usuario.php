758218646fdb8a5b7dec4f537b890ccfd24c0e1d
Dilan <dfgaleano69@misena.edu.co>
2022-03-10 08:33:24 -0500

    Final

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,52 +1,104 @@
 <?php
+
 namespace App\Models;
-class Usuario
+use App\Enums\Estado;
+
+use App\Enums\Roll;
+use App\Interfaces\Model;
+use Exception;
+
+
+require_once ("AbstractDBConnection.php");
+require_once (__DIR__."\..\Interfaces\Model.php");
+require_once (__DIR__.'/../../vendor/autoload.php');
+
+use JetBrains\PhpStorm\Internal\TentativeType;
+
+class Usuario extends AbstractDBConnection implements Model
 {
-   private  ? int $IdUsuario;
-   private int $documento;
-   private String  $nombre;
-   private String $telefono;
-   private String  $direccion;
-   private String  $roll;
-   private String  $contraseña;
+
+
+    private ? int $idUsuario;
+    private int $documento;
+    private string $nombres;
+    private String $telefono;
+    private string $direccion;
+    private Roll $roll;
+    private ?string $usuario;
+    private ?string $contrasena;
+    private Estado $estado;
+    private ?int $municipiosId;
+
+    //Realaciones
+    private ? Municipio $municipios;
+    private ?array $FabricacionUsuario;
+    private ?array $FacturaUsurio;
+
+    /* Seguridad de Contraseña */
+    const HASH = PASSWORD_DEFAULT;
+    const COST = 10;
 
     /**
-     * @param int|null $IdUsuario
-     * @param int $documento
-     * @param String $nombre
-     * @param String $telefono
-     * @param String $direccion
-     * @param String $roll
-     * @param String $contraseña
+     * Usuarios constructor. Recibe un array asociativo
+     * @param array $usuario
      */
-    public function __Usuario(){
-
-     }
-    public function __construct(?int $IdUsuario, int $documento, string $nombre, string $telefono, string $direccion, string $roll, string $contraseña)
+    public function __construct(array $usuario = [])
     {
-        $this->IdUsuario = $IdUsuario;
-        $this->documento = $documento;
-        $this->nombre = $nombre;
-        $this->telefono = $telefono;
-        $this->direccion = $direccion;
-        $this->roll = $roll;
-        $this->contraseña = $contraseña;
+        parent::__construct();
+        $this->setIdUsuario($usuario['idUsuario'] ?? null);
+        $this->setDocumento($usuario['documento'] ?? 0);
+        $this->setNombres($usuario['nombre'] ?? '');
+        $this->setTelefono($usuario['telefono'] ?? '');
+        $this->setDireccion($usuario['direccion'] ?? '');
+        $this->setRoll($usuario['roll'] ?? Roll::ADMINISTRADOR);
+        $this->setUsuario($usuario['usuario'] ?? NULL);
+        $this->setContrasena($usuario['contrasena'] ?? NULL);
+        $this->setEstado($usuario['estado'] ?? Estado::INACTIVO);
+        $this->setMunicipiosId($usuario['municipiosId']??null);
     }
+
+    public function __destruct()
+    {
+        if ($this->isConnected()) {
+            $this->Disconnect();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsuario(): ?string
+    {
+        return ucwords($this->usuario);
+    }
+
+    /**
+     * @param string $usuario
+     */
+    public function setUsuario(?string $usuario): void
+    {
+        if($usuario == null){
+            $this->usuario = $usuario;
+        }else{
+            $this->usuario = strtolower($usuario);
+        }
+    }
+
 
     /**
      * @return int|null
      */
     public function getIdUsuario(): ?int
     {
-        return $this->IdUsuario;
+        return $this->idUsuario;
     }
 
     /**
-     * @param int|null $IdUsuario
+     * @param int|null $idUsuario
      */
-    public function setIdUsuario(?int $IdUsuario): void
+    public function setIdUsuario(?int $idUsuario): void
     {
-        $this->IdUsuario = $IdUsuario;
+        $this->idUsuario = $idUsuario;
     }
 
     /**
@@ -68,21 +120,21 @@ class Usuario
     /**
      * @return string
      */
-    public function getNombre(): string
+    public function getNombres(): string
     {
-        return $this->nombre;
+        return ucwords($this->nombres);
     }
 
     /**
-     * @param string $nombre
+     * @param string $nombres
      */
-    public function setNombre(string $nombre): void
+    public function setNombres(string $nombres): void
     {
-        $this->nombre = $nombre;
+      $this->nombres = trim(mb_strtolower($nombres, 'UTF-8'));
     }
 
     /**
-     * @return string
+     * @return String
      */
     public function getTelefono(): string
     {
@@ -90,7 +142,7 @@ class Usuario
     }
 
     /**
-     * @param string $telefono
+     * @param String $telefono
      */
     public function setTelefono(string $telefono): void
     {
@@ -102,7 +154,7 @@ class Usuario
      */
     public function getDireccion(): string
     {
-        return $this->direccion;
+        return ucwords($this->direccion);
     }
 
     /**
@@ -114,76 +166,128 @@ class Usuario
     }
 
     /**
-     * @return string
+     * @return Roll
      */
     public function getRoll(): string
     {
-        return $this->roll;
+        return ucwords($this->roll->toString());
     }
 
     /**
-     * @param string $roll
+     * @param Roll $roll
      */
-    public function setRoll(string $roll): void
+    public function setRoll(null|string|Roll $roll): void
     {
-        $this->roll = $roll;
+        if(is_string($roll)){
+            $this->roll = Roll::from($roll);
+
+        }else{
+            $this->roll = $roll;
+        }
+    }
+
+
+
+    /**
+     * @return string|null
+     */
+    public function getContrasena(): ?string
+    {
+        return $this->contrasena;
     }
 
     /**
-     * @return string
+     * @param string|null $contrasena
      */
-    public function getContraseña(): string
+    public function setContrasena(?string $contrasena): void
     {
-        return $this->contraseña;
+        $this->contrasena = $contrasena;
     }
 
     /**
-     * @param string $contraseña
+     * @return Estado
      */
-    public function setContraseña(string $contraseña): void
+    public function getEstado(): string
     {
-        $this->contraseña = $contraseña;
+        return ucwords($this->estado->toString());
     }
 
+    /**
+     * @param EstadoCategorias|null $estado
+     */
+    public function setEstado(null|string|Estado $estado): void
+    {
+        if(is_string($estado)){
+            $this->estado = Estado::from($estado);
+        }else{
+            $this->estado = $estado;
+        }
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMunicipiosId(): ?int
+    {
+        return $this->municipiosId;
+    }
+
+    /**
+     * @param int|null $municipiosId
+     */
+    public function setMunicipiosId(?int $municipiosId): void
+    {
+        $this->municipiosId = $municipiosId;
+    }
+
+    /**
+     * @return Municipio|null
+     */
+    public function getMunicipio(): ?Municipio
+    {
+        if(!empty($this->municipiosId)){
+            $this->municipios = Municipio::searchForId( $this->municipiosId) ?? new Municipio();
+            return $this->municipios;
+        }
+        return null;
+    }
 
     /**
      * @param string $query
      * @return bool|null
-     * metodo para guardar un abono
      */
     protected function save(string $query): ?bool
-
     {
+        if($this->contrasena != ' ' ){
+            $hashPassword = password_hash($this->contrasena, self::HASH, ['cost' => self::COST]);
+        }
         $arrData = [
-            ':IdUsuario' =>    $this->getIdUsuario(),
-            ':documento' =>   $this->getdocumento(),
-            ':nombre' =>   $this->getnombre(),
-            ':telefono' =>   $this->gettelefono(),
-            ':direccion' =>   $this->getdireccion(),
-            ':roll' =>   $this->getroll(),
-            ':contraseña' =>   $this->getcontraseña(),
+            ':idUsuario' => $this->getIdUsuario(),
+            ':documento' => $this->getDocumento(),
+            ':nombre' =>   $this->getNombres(),
+            ':telefono' =>   $this->getTelefono(),
+            ':direccion' =>   $this->getDireccion(),
+            ':roll' =>   $this->getRoll(),
+            ':usuario' => $this->getUsuario(),
+            ':contrasena' =>   $hashPassword,
+            ':estado' =>   $this->getEstado(),
+            ':municipiosId' => $this->getMunicipiosId()
         ];
-
-
         $this->Connect();
         $result = $this->insertRow($query, $arrData);
         $this->Disconnect();
         return $result;
     }
 
-
-
     /**
-     * @param string $query
      * @return bool|null
-     * metodo para guardar un abono
      */
     public function insert(): ?bool
     {
-        $query = "INSERT INTO proyecto.usuarios VALUES (
-            :id,:nombres,:apellidos,:tipo_documento,:documento,
-            :telefono,:direccion,:municipio_id,:fecha_nacimiento,:user,
-            :password,:foto,:rol,:estado,:created_at,:updated_at
+        $query = "INSERT INTO ornamentacion.usuario VALUES (
+            :idUsuario,:documento,:nombre,
+            :telefono,:direccion,:roll,:usuario,
+            :contrasena,:estado,:municipiosId
         )";
         return $this->save($query);
     }
@@ -193,12 +297,9 @@ class Usuario
      */
     public function update(): ?bool
     {
-        $query = "UPDATE weber.usuarios SET 
-            nombres = :nombres, apellidos = :apellidos, tipo_documento = :tipo_documento, 
-            documento = :documento, telefono = :telefono, direccion = :direccion, 
-            municipio_id = :municipio_id, fecha_nacimiento = :fecha_nacimiento, user = :user,  
-            password = :password, foto = :foto, rol = :rol, estado = :estado, created_at = :created_at, 
-            updated_at = :updated_at WHERE id = :id";
+        $query = "UPDATE ornamentacion.usuario SET 
+            documento = :documento,nombre = :nombre, telefono = :telefono, direccion = :direccion,roll = :roll,usuario = :usuario, 
+            contrasena = :contrasena, estado = :estado , municipiosId = :municipiosId  WHERE idUsuario = :idUsuario";
         return $this->save($query);
     }
 
@@ -211,30 +312,147 @@ class Usuario
         $this->setEstado("Inactivo"); //Cambia el estado del Usuario
         return $this->update();                    //Guarda los cambios..
     }
+
     /**
      * @param $query
-     * @return Categorias|array
+     * @return Usuario|array
      * @throws Exception
      */
     public static function search($query) : ?array
     {
         try {
-            $arrUsuarios = array();
+            $arrUsuario = array();
             $tmp = new Usuario();
             $tmp->Connect();
             $getrows = $tmp->getRows($query);
             $tmp->Disconnect();
 
-            foreach ($getrows as $valor) {
-                $Usuario= new Categorias($valor);
-                array_push($arrUsuarios, $Usuario);
-                unset($Usuario);
+            if (!empty($getrows)) {
+                foreach ($getrows as $valor) {
+                    $Usuario = new Usuario($valor);
+                    array_push($arrUsuario, $Usuario);
+                    unset($Usuario);
+                }
+                return $arrUsuario;
             }
-            return $arrUsuarios;
+            return null;
         } catch (Exception $e) {
-            GeneralFunctions::logFile('Exception',$e, 'error');
+            GeneralFunctions::logFile('Exception', $e);
         }
         return null;
+    }
+
+    /**
+     * @param int $id
+     * @return Usuario|null
+     */
+    public static function searchForId(int $id): ?Usuario
+    {
+        try {
+            if ($id > 0) {
+                $tmpUsuario = new Usuario();
+                $tmpUsuario->Connect();
+                $getrow = $tmpUsuario->getRow("SELECT * FROM ornamentacion.usuario WHERE idUsuario =?", array($id));
+                $tmpUsuario->Disconnect();
+                return ($getrow) ? new Usuario($getrow) : null;
+            } else {
+                throw new Exception('Id de usuario Invalido');
+            }
+        } catch (Exception $e) {
+            GeneralFunctions::logFile('Exception', $e);
+        }
+        return null;
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public static function getAll(): array
+    {
+        return Usuario::search("SELECT * FROM ornamentacion.usuario");
+    }
+
+    /**
+     * @param $documento
+     * @return bool
+     * @throws Exception
+     */
+    public static function usuarioRegistrado($documento): bool
+    {
+        $result = usuario::search("SELECT * FROM ornamentacion.usuario where documento = " . $documento);
+        if (!empty($result) && count($result)>0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function nombresCompletos(): string
+    {
+        return $this->nombres . " " ;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return "Nombre: $this->nombres, 
+                Documento: $this->documento, 
+                Telefono: $this->telefono, 
+                Direccion: $this->direccion, 
+                ";
+    }
+
+    public function login($usuario, $password): Usuario|String|null
+    {
+
+        try {
+            $resultUsuario = Usuario::search("SELECT * FROM ornamentacion.usuario WHERE usuario = '$usuario'");
+            /* @var $resultUsuario Usuario[] */
+            if (!empty($resultUsuario) && count($resultUsuario) >= 1) {
+                if (password_verify($password, $resultUsuario[0]->getContrasena())) {
+                    if ($resultUsuario[0]->getEstado() == 'Activo') {
+                        return $resultUsuario[0];
+                    } else {
+                        return "Usuario Inactivo";
+                    }
+                } else {
+                    return "Contraseña Incorrecta";
+                }
+            } else {
+                return "Usuario Incorrecto";
+            }
+        } catch (Exception $e) {
+            GeneralFunctions::logFile('Exception', $e);
+            return "Error en Servidor";
+        }
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return array data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'idUsuario' => $this->getIdUsuario(),
+            'documento' => $this->getDocumento(),
+            'nombre' =>  $this->getNombres(),
+            'telefono' =>   $this->getTelefono(),
+            'direccion' =>   $this->getDireccion(),
+            'roll' =>   $this->getRoll(),
+            'contrasena'=> $this-> getContrasena() ,
+            'estado' =>   $this->getEstado(),
+            'municipiosId' => $this->getMunicipiosId(),
+        ];
     }
 
 
